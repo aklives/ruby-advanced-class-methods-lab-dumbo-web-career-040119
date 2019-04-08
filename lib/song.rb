b78efbc87210039132e8360655ccc8023e95d5b3
Adam Kearney <aklives@hotmail.com>
2019-04-08 08:02:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,6 +48,13 @@ class Song
     song
   end
       
-    
+   
+  def self.create_from_filename
+    song = self.new
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    @@all << song
+    song
+  end
   
 end
